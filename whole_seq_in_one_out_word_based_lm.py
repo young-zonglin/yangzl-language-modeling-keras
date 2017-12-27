ca0af714083772c02ef543e36b1430e59b959314
yangzonglin <806765278@qq.com>
2017-12-27 21:15:24 +0800

    事实上，one-hot编码输出，得到的二维数组每个元素是8个字节

--- whole_seq_in_one_out_word_based_lm.py
+++ whole_seq_in_one_out_word_based_lm.py
@@ -15,7 +15,7 @@ import sys
 # TODO 使用序列标注的方法减少词汇表的大小
 # fix seed of generator of random number for reproducibility
 numpy.random.seed(7)
-train_text_url = 'E:\自然语言处理数据集\搜狐新闻数据(SogouCS)_segment\\news.sohunews.010801.txt.utf-8.xml.train.seq.clean.gbk.segment'
+train_text_url = 'E:\\222.txt'
 # ['content of file1', ..., 'content of file2']
 raw_corpus_data = []
 with open(train_text_url, 'r', encoding='gbk') as train_file:
@@ -34,10 +34,10 @@ for text in raw_corpus_data:
             input_output_pairs.append(input_output_pair)
 print('Total number of input-output pair: {}'.format(len(input_output_pairs)))
 y_shape = len(input_output_pairs), vocab_size+1
-y_memory_size = tools.get_array_memory_size(y_shape, 1/8)
+y_memory_size = tools.get_array_memory_size(y_shape, item_size=8)
 print('one-hot编码后的输出占用内存大小为：', y_memory_size, 'GB')
 if y_memory_size > parameters.Y_MEMORY_SIZE_THRESHOLD_GB:
-    print('内存占用超过', parameters.Y_MEMORY_SIZE_THRESHOLD_GB, '2GB')
+    print('内存占用超过', parameters.Y_MEMORY_SIZE_THRESHOLD_GB, 'GB')
     sys.exit(0)
 # pad input sequences
 max_length = max([len(input_output_pair) for input_output_pair in input_output_pairs])
