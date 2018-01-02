a4915631cd1ef23f0b4108fd423adb0538db44d5
yangzonglin <806765278@qq.com>
2018-01-02 18:44:34 +0800

    获取项目根目录和训练文件所在目录

--- parameters.py
+++ parameters.py
@@ -1,1 +1,12 @@
+import os
+
+
 Y_MEMORY_SIZE_THRESHOLD_GB = 2
+# TODO 学习os模块
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+TRAIN_DATA_PATH = os.path.join(PROJECT_ROOT, 'data')
+
+
+if __name__ == '__main__':
+    print(PROJECT_ROOT)
+    print(TRAIN_DATA_PATH)
